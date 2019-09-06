60d29101015476acb2c72bb2c48c1cdf732275ac
joannaarroyo <joliuac@gmail.com>
2019-09-05 23:34:50 -0700

    added roles to newRouter, updated README

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,12 +8,12 @@ module.exports = capability => {
       let [authType, authString] = req.headers.authorization.split(/\s+/);
 
       switch (authType.toLowerCase()) {
-        case 'basic':
-          return _authBasic(authString);
-        case 'bearer':
-          return _authBearer(authString);
-        default:
-          return _authError();
+      case 'basic':
+        return _authBasic(authString);
+      case 'bearer':
+        return _authBearer(authString);
+      default:
+        return _authError();
       }
     } catch (e) {
       _authError();
@@ -48,7 +48,7 @@ module.exports = capability => {
     }
 
     function _authError() {
-      next("Invalid User ID / Password");
+      next('Invalid User ID / Password');
     }
-  }
+  };
 };
--- src/newRouter.js
+++ src/newRouter.js
@@ -7,10 +7,18 @@ const User = require('./model/user.js');
 const Article = require('./model/article.js');
 const auth = require('./middleware/auth.js');
 const oauth = require('./oauth/google.js');
+const Role = require('./model/role');
 
-newRouter.get('/public-stuff', auth, (req, res, next) => {
-  res.send({user: req.user, message: 'Can read'});
+newRouter.get('/create-roles', (req, res, next) => {
+  const admin = new Role({role: 'admin', capabilities: ['create', 'read', 'update', 'delete']});
+  const editor = new Role({role: 'editor', capabilities: ['create', 'read', 'update']});
+  const user = new Role({role: 'user', capabilities: ['read']});
 });
+
+
+// newRouter.get('/something-to-read', auth('read'), (req, res, next) => {
+//   res.send({user: req.user, message: 'Can read'});
+// });
 
 newRouter.post('/create-a-thing', auth('create'), (req, res, next) => {
   res.send({user: req.user, message: 'Can post'});
@@ -19,5 +27,9 @@ newRouter.post('/create-a-thing', auth('create'), (req, res, next) => {
 newRouter.put('/update', auth('update'), (req, res, next) => {
   res.send({user: req.user, message: 'Can put'});
 });
+
+// newRouter.delete('/bye', auth('delete'), (req, res, next) => {
+//   res.send({user: req.user, message: 'Can delete'});
+// });
 
 module.exports = newRouter;
